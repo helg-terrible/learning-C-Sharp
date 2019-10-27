5ee93b4916a2439a08c9fd06e4b4a92c2d1c1b78
OlegR <paladinss44@rambler.ru>
2019-10-27 22:05:24 +0300

    add Hello World on VS Code

---
+++ Hellow World/MyClass.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Hellow_World
+{
+    public class MyClass
+    {
+        public string ReturnMessage()
+        {
+            return "Happy coding!";
+        }
+    }
+}
