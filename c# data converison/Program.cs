af7ecdd9bb9e6fdad172e98f8c295eaef76d495a
OlegR <paladinss44@rambler.ru>
2019-10-31 00:08:27 +0300

    add data conversion

---
+++ c# data converison/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace c__data_conversation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // convert bool to string
+            bool boolValue = true;
+            string stringValue = Convert.ToString(boolValue);
+            Console.WriteLine(boolValue);
+
+            // string to list of chars (arrays)
+            string someString = "abcdfghght";
+            var charsList = someString.ToCharArray();
+            Console.WriteLine("Original string: {0}", someString);
+            Console.WriteLine("Character array: ");
+            for (int ctr = 0; ctr < charsList.Length; ctr++)
+                Console.WriteLine("     {0}: {1}", ctr, charsList[ctr]);
+
+            // 
+
+        }
+    }
+}
