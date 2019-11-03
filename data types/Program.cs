b339563d7045ebbebc60d436b14911601515e271
OlegR <paladinss44@rambler.ru>
2019-11-03 07:22:32 +0300

    add work with data types examples

---
+++ data types/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace data_types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // shortcut expression of addition and sustrction
+            int apple = 0;
+            apple ++;
+            apple --;
+            apple += 3; // or apple -= 3;
+
+            Console.WriteLine(apple);
+            
+        }
+    }
+}
