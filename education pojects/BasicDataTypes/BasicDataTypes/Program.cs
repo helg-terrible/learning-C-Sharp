2714dc5ba9cee658b5e04fcef672d48e364f1fd8
OlegR <paladinss44@rambler.ru>
2019-12-18 22:07:58 +0300

    добавил примеры парсера из string в стандартные типы

--- education pojects/BasicDataTypes/BasicDataTypes/Program.cs
+++ education pojects/BasicDataTypes/BasicDataTypes/Program.cs
@@ -15,6 +15,9 @@ namespace BasicDataTypes
             ObjectFunctionality();
             DataTypeFunctionality();
             CharFunctionality();
+            ParseFromString();
+            ParseFromStringsWithTryParse();
+
             Console.ReadLine();
         }
 
@@ -85,6 +88,42 @@ namespace BasicDataTypes
             Console.WriteLine("char.IsPunctuation(’?'): {0}", char.IsPunctuation('?'));
             Console.WriteLine();
         }
+
+        static void ParseFromString()
+        {
+            Console.WriteLine("=> Data type parsing: "); 
+            bool b = bool.Parse("True");
+            Console.WriteLine("Value of b: {0} ", b); // Вывод значения b
+            //double d = double.Parse("99.884");
+            //Console.WriteLine("Value of d: {0} ", d); // Вывод значения d
+            int i = int.Parse("8") ;
+            Console.WriteLine("Value of l: {0}", i); // Вывод значения i
+            //char c = Char.Parse("w");
+            //Console.WriteLine("Value of c: {о}", c); // Вывод значения с
+            Console.WriteLine();
+        }
+
+        static void ParseFromStringsWithTryParse()
+        {
+            Console.WriteLine("=> Data type parsing with TryParse:");
+            if (bool.TryParse("True", out bool b)) ;
+            {
+                Console.WriteLine("Value of b: {0}", b);  // Вывод значения b
+            }
+            
+            string value = "Hello";
+            if (double.TryParse(value, out double d))
+            {
+                Console.WriteLine("Value of d: {0}", d);  // Вывод значения d
+            }
+            else
+            {
+                // Преобразование потерпело неудачу
+                Console.WriteLine("Failed to convert the input ({0}) to a double", value);
+            }
+            Console.WriteLine();
+        }
+      
     }
 }
 
