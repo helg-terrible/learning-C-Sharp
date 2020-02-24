5f75cc2a70889030696f89672d42a2648f1fc901
OlegR <paladinss44@rambler.ru>
2020-02-24 12:45:14 +0300

    домашнее задание №6. Реализовано: - добавление записи - удаление записи

---
+++ _skillbox unity homework/Homework_6/Event.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_6
+{
+    public enum eventType
+    {
+        Personal,
+        Job,
+        Health,
+        Other
+    }
+
+    public class Event
+    {
+        // fields
+        private DateTime startOfEvent; // start of event
+        private DateTime endOfEvent; // end of event
+        private string eventTitile; // title of event
+        private string eventDescription; // event desc
+        private eventType eventType; // type of Event (personal deal, job, chil or other)
+                                     // private Event parentEvenet; // parent event (link to another event)
+
+
+        #region ctors
+        /// <summary>
+        /// type of event other
+        /// </summary>
+        public Event() : this(new DateTime(), new DateTime(), default, default, eventType.Other) { }
+
+        // конструктор с типом записи other по-умолчанию
+        public Event(DateTime startOfEvent, DateTime endOfEvent, string eventTitile) :
+            this(startOfEvent, endOfEvent, eventTitile, "", eventType.Other) { }
+
+        public Event(DateTime startOfEvent, DateTime endOfEvent, string eventTitile, string eventDescription, eventType eventType)
+        {
+            this.startOfEvent = startOfEvent;
+            this.endOfEvent = endOfEvent;
+            this.eventTitile = eventTitile;
+            this.eventDescription = eventDescription;
+            this.eventType = eventType;
+        }
+
+        #endregion
+
+
+        #region Properties
+        public DateTime StartOfEvent
+        {
+            get { return startOfEvent; }
+            set { startOfEvent = value; }
+        }
+
+        public DateTime EndOfEvent
+        {
+            get { return endOfEvent; }
+            set { endOfEvent = value; }
+        }
+
+        public string EventTitile
+        {
+            get { return eventTitile; }
+            set { eventTitile = value; }
+        }
+
+        public string EventDescription
+        {
+            get { return eventDescription; }
+            set { eventDescription = value; }
+        }
+
+        public eventType EventType 
+        { 
+            get { return eventType; } 
+            set { eventType = value; }
+        }
+
+        #endregion
+
+
+        #region Methods
+        public void Print()
+        {
+            Console.WriteLine($"{this.startOfEvent, 20} {this.endOfEvent, 20} " +
+                                $"{this.eventTitile, 20} {this.eventDescription, 20} {this.eventType, 5}");
+        }
+
+        #endregion
+
+    }
+}
---
+++ _skillbox unity homework/Homework_6/ScheduleEvents.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_6
+{
+    public class ScheduleEvents
+    {
+        #region fields
+        private List<Event> eventList = new List<Event>();
+
+        private static string[] titles = { "Start of event", "End of event", "Title", "Description", "Event's type" };
+
+        #endregion
+
+        #region ctors
+        public ScheduleEvents() {}
+
+        #endregion
+
+        #region properties
+
+
+        #endregion
+
+        #region methods
+        /// <summary>
+        /// добавляет новый объект типа Event в список
+        /// </summary>
+        /// <param name="startOfEvent">формат yyyy mm dd hh mm ss</param>
+        /// <param name="endOfEvent">формат yyyy mm dd hh mm ss</param>
+        /// <param name="eventTitle">заголовок события, не более 15 символов</param>
+        /// <param name="eventDescription">описание события, не более 300 символов</param>
+        /// <param name="eventType">тип события</param>
+        public void AddItem(string strStartOfEvent, string strEndOfEvent, string strEventTitle, string strEventDescription, string strEventType)
+        {
+            eventList.Add(new Event()
+            {
+                StartOfEvent = GetDateTime(strStartOfEvent),
+                EndOfEvent = GetDateTime(strEndOfEvent),
+                EventTitile = strEventTitle,
+                EventDescription = strEventDescription,
+                EventType = GetEventType(strEventType)
+            });
+        }
+
+        /// <summary>
+        /// возвращает объект дата время из строки
+        /// </summary>
+        /// <param name="dateTime">строка с датой в формате yyyy mm dd hh mm ss </param>
+        /// <returns></returns>
+        private DateTime GetDateTime(string strdDateTime)
+        {
+            // получим из строки с датой массив: год, месяц, день, часы, минуты, секунды
+            string[] partsOfDateTime = strdDateTime.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+
+            try
+            {
+                return new DateTime(int.Parse(partsOfDateTime[0]),
+                                        int.Parse(partsOfDateTime[1]),
+                                        int.Parse(partsOfDateTime[2]),
+                                        int.Parse(partsOfDateTime[3]),
+                                        int.Parse(partsOfDateTime[4]),
+                                        int.Parse(partsOfDateTime[5]));
+            }
+
+            catch { return new DateTime(); }
+                
+
+        }
+        /// <summary>
+        /// по строке определяет тип события
+        /// </summary>
+        /// <param name="strEventType">Personal, Job, Health, Other</param>
+        /// <returns>возвращает объект типа eventType</returns>
+        private eventType GetEventType(string strEventType)
+        {
+            switch (strEventType)
+            {
+                case "Personal":
+                    return eventType.Personal;
+                    break;
+                case "Job": 
+                    return eventType.Job;
+                    break;
+                case "Health":
+                    return eventType.Health;
+                    break;
+                default:
+                    return eventType.Other;
+                    break;
+            }
+        }
+
+        public void RemoveItem(int index)
+        {
+            eventList.RemoveAt(index);
+        }
+        #endregion
+
+    }
+}
